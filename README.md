48ebd492dfab963f61d8686e1e226dba748f2f4d
izhaoge@gmail.com <izhaoad@gmail.com>
2017-12-02 11:22:21 +0800

    Update the README

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
-# xfly
-This is SPA framework for the lite Web App.
+# Xfly
+This is SPA framework for the lite Web App with Morden browser.
+
+# Features
+* Page life-cycle supported(refer Android、iOS);
+* SEO friendly;
+* Pre-load;
+* Lazy load;
+* Multi-instance;
+* Single page reload;
+* Easy way for Query string accessing;
+
+
+
 
 # Examples
 See the examples dir.
+
+
+# Legacy version is here(Hash based)
+Click to get the [base.fly](https://github.com/c-ong/starter/tree/dev/app/scripts).
--- examples/js/app/welcome.js
+++ examples/js/app/welcome.js
@@ -4,13 +4,13 @@ $Page.define('welcome', {
 
     html: '<div class="page-ui">' +
             '<h1>Hello</h1>' +
-            '<p>If you are use Server side rendering(PHP with Nginx、Apache...), here no need the index.html file.</p>' +
+            '<p>If you are using Server side render(PHP with Nginx、Apache...), here is no need the index.html file.</p>' +
             '<a class="xfly-page__nav" href="see/you/again">Go to the Next page</a>' +
             '<a class="xfly-page__nav" href="page/with/args?key=Xfly">Go to the Next page with Args</a>' +
             '<br/>' +
             '<br/>' +
             '<br/>' +
-            '<b>MORE EXAMPLE BE COMING SOON...</b>' +
+            '<b>MORE EXAMPLES BE COMING SOON...</b>' +
           '</div>',
 
     onCreateView: function() {
